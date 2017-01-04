a530569194329db780c7449ea17f36b7b535a6d4
jeffa <jeffa@unlocalhost.com>
2017-01-04 14:48:49 -0500

    prep for next version

--- lib/Template/Plugin/Calendar/Simple.pm
+++ lib/Template/Plugin/Calendar/Simple.pm
@@ -1,7 +1,7 @@
 package Template::Plugin::Calendar::Simple;
 use strict;
 use warnings FATAL => 'all';
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 use Calendar::Simple;
 use Template::Plugin;
